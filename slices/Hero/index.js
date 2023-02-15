b45e017610a7d2e905871acab2dfcc027f98af38
Louise Findlay <louise.findlay@prismic.io>
2023-02-15 17:18:26 +0000

    Refactored hero section to use CSS Grid

--- slices/Hero/index.js
+++ slices/Hero/index.js
@@ -1,29 +1,26 @@
 import React from "react";
+import Image from "next/image";
 import { PrismicRichText } from "@prismicio/react";
 import styles from "./hero.module.css";
 
 const Hero = ({ slice }) => (
-    <section
-        className={`${styles.heroBackground} ${styles.flexVerticalCenter}`}
-    >
-        <div className={`${styles.heroContent} ${styles.flexVerticalCenter}`}>
-            {/* Hero Content */}
+    <section className={`${styles.heroBackground}`}>
+        {/* Hero Content */}
+        <div className={`${styles.heroContent}`}>
             <PrismicRichText field={slice.primary.hero_heading} />
             <PrismicRichText field={slice.primary.hero_text} />
             <button>
                 <PrismicRichText field={slice.primary.hero_button_text} />
             </button>
         </div>
+        {/* TODO: Get PrismicImage or PrismicNextImage working */}
+        <Image
+            src={slice.primary.hero_image.url}
+            alt={""}
+            width={1920}
+            height={100}
+        />
     </section>
 );
 
 export default Hero;
-
-{
-    /* TODO: Refactor background image to CSS Grid to set URL as fallback and background image as Prismic Image field */
-}
-
-<div class="grid">
-    <img />
-    <div class="text"></div>
-</div>;
