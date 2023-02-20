799804fb577412818128b92eb8ef3af90fb9e2e5
Louise Findlay <louise.findlay@prismic.io>
2023-02-20 11:01:26 +0000

    Fixed title rendering

--- components/layout.js
+++ components/layout.js
@@ -1,18 +1,17 @@
 import React from "react";
-import { PrismicText } from "@prismicio/react";
+import * as prismicH from "@prismicio/helpers";
 import Head from "next/head";
 import Header from "./Header";
 import Footer from "./Footer";
 import { CurrencyProvider } from "../context/currencyData";
 
 export default function Layout({ children, page, navigation }) {
+    const siteTitle = prismicH.asText(page.data.site_title);
     return (
         <>
             <CurrencyProvider>
                 <Head>
-                    <title>
-                        <PrismicText field={page.data.site_title} />
-                    </title>
+                    <title>{siteTitle}</title>
                 </Head>
                 <Header page={page} navigation={navigation}></Header>
                 <main>{children}</main>
--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ import Layout from "../components/Layout";
 export default function Home({ page, navigation, settings }) {
     return (
         <Layout page={page} navigation={navigation}>
-            <SliceZone slices={page.data.slices} components={components} />;
+            <SliceZone slices={page.data.slices} components={components} />
         </Layout>
     );
 }
